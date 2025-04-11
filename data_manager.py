95379ec12b04bd06f965e072f10c3b2a985b46ed
Agnieszka26 <medrek.agn@yahoo.com>
2025-04-11 08:15:32 +0200

    Step 4 - Retrieve your customer emails

--- data_manager.py
+++ data_manager.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 SHEETY_TOKEN = os.getenv("SHEETY_TOKEN")
-URL_SHEETY = "https://api.sheety.co/cc0f67e3ac3f79b768d391db8f872fd0/flightDeals/prices"
+URL_SHEETY_PRICES = os.getenv("URL_SHEETY_PRICES")
+URL_SHEETY_USERS = os.getenv("URL_SHEETY_USERS")
 
 sheety_headers = {
     "Authorization": f"Basic {SHEETY_TOKEN}"
@@ -15,23 +16,33 @@ class DataManager:
         self.past_lowest_prices = self.get_lowest_prices()["prices"]
 
     def get_lowest_prices(self):
-        sheety_response = requests.get(url=URL_SHEETY, headers=sheety_headers)
+        sheety_response = requests.get(url=URL_SHEETY_PRICES , headers=sheety_headers)
         return sheety_response.json()
-    def update_iata_code(self, id, iata_code):
+    def update_iata_code(self, row_id, iata_code):
         body = {
             "price": {
                "iataCode": iata_code
             }
         }
-        sheety_update_row = requests.put(url=f"{URL_SHEETY}/{id}", json=body, headers=sheety_headers)
+        sheety_update_row = requests.put(url=f"{URL_SHEETY_PRICES}/{row_id}", json=body, headers=sheety_headers)
         sheety_update_row.raise_for_status()
         print(sheety_update_row.text)
-    def update_price(self, id, price):
+    def update_price(self, row_id, price):
         body = {
             "price": {
                "lowestPrice": price
             }
         }
-        sheety_update_row = requests.put(url=f"{URL_SHEETY}/{id}", json=body, headers=sheety_headers)
+        sheety_update_row = requests.put(url=f"{URL_SHEETY_PRICES }/{row_id}", json=body, headers=sheety_headers)
         sheety_update_row.raise_for_status()
         print(sheety_update_row.text)
+
+    def get_customer_emails(self):
+        response = requests.get(url=URL_SHEETY_USERS, headers=sheety_headers)
+        response.raise_for_status()
+        users = response.json()['users']
+        emails = []
+        for user in users:
+            email = user['whatIsYourEmailAddress ?']
+            emails.append(email)
+        return emails
